e7f00f3fe717f8e139b74af21cb33a9321f514c4
savagerunderscorelex <eigbekoyi@gmail.com>
2025-09-19 16:25:26 -0400

    started on typing and deleting function in javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+let stateLife = "life";
+let stateLove = "love";
+let stateEverything = "everything";
+let stateILoveMusic = "i love music";
+
+let speed = 100;
+
+
+let i = -1;
+
+let cursor = document.getElementById("cursor");
+
+function typeAboutMusic() {
+    if (i > stateLife){
+    document.getElementById("third-word").innerHTML -= document.getElementById("site_heading").innerHTML.charAt(i);
+    i
+    setTimeout(typeAboutMusic, speed);
+    }
+    
+}
+
+window.onload = typeAboutMusic;
